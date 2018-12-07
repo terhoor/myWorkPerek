f54ddf30648be2664334c6e665ce9464e0675b7e
terhoor <terhoor@yandex.ru>
2018-12-07 16:19:32 +0400

    add form

--- src/app/code-access-page/code-access-page.component.ts
+++ src/app/code-access-page/code-access-page.component.ts
@@ -9,6 +9,9 @@ import { OtherDataService } from '../shared/services/other-data.service';
 })
 export class CodeAccessPageComponent implements OnInit {
   phoneNumber: string;
+  isValidForm: boolean;
+  timer: number = 2;
+  timerEnd: boolean = false;
   formCode = this.fb.group({
     code: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(4)]]
   });
@@ -21,6 +24,28 @@ export class CodeAccessPageComponent implements OnInit {
 
       this.phoneNumber = numberStr.replace(/^(\+\d)(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/, '$1 ($2) $3-$4-$5');
     });
+    
+    this.timerTick();
+  }
+
+  timerTick() {
+    const myTimer = setInterval(() => {
+
+      if (this.timer === 0) {
+        clearTimeout(myTimer);
+        this.timerEnd = true;
+        return;
+      }
+      this.timer--;
+    }, 1000);
+
+    console.log(this.timer);
+  }
+
+  startTimer() {
+    this.timerEnd = false;
+    this.timer = 2;
+    this.timerTick();
   }
 
 }
