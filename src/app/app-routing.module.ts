9783a701096258627b2bce8ee04672e84fe7b181
terhoor <terhoor@yandex.ru>
2018-12-07 15:25:58 +0400

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 import { InfoComponent } from './info/info.component';
 import { ConsentPageComponent } from './consent-page/consent-page.component';
+import { CodeAccessPageComponent } from './code-access-page/code-access-page.component';
 
 const appRoutes: Routes = [
   {path: '', component: MainLayoutComponent, children: [
     {path: '', redirectTo: '/info', pathMatch: 'full'},
     {path: 'info', component: InfoComponent},
     {path: 'consent', component: ConsentPageComponent},
+    {path: 'code', component: CodeAccessPageComponent},
   ]},
-  {path: '**', redirectTo: '/404'}
+  {path: '**', redirectTo: '/info'}
 
 ];
 
