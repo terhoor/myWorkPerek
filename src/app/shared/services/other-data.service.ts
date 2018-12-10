15ae73e057a934346104cdca96b81f3c5c2bf730
terhoor <terhoor@yandex.ru>
2018-12-10 16:59:20 +0400

    change service

--- src/app/shared/services/other-data.service.ts
+++ src/app/shared/services/other-data.service.ts
@@ -1,6 +1,7 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { post } from 'selenium-webdriver/http';
 
 @Injectable()
 export class OtherDataService {
@@ -11,16 +12,23 @@ export class OtherDataService {
 
   }
 
-  getCode() {
-    return this.httpService.get('../assets/code.json');
-  }
 
   changeNumberDecore(numberStr) {
     if (numberStr.length === 12) {
-    return numberStr.replace(/^(\+\d)(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/, '$1 ($2) $3-$4-$5');
-  } else if (numberStr.length === 10) {
-    return numberStr.replace(/^(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/, '($1) $2-$3-$4');
+      return numberStr.replace(/^(\+\d)(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/, '$1 ($2) $3-$4-$5');
+    } else if (numberStr.length === 10) {
+      return numberStr.replace(/^(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/, '($1) $2-$3-$4');
+    }
+  }
+
+  // при настройке api изменить с все api
+
+  getCheckCode(): Observable<Object> {
+    return this.httpService.get('../assets/code.json');
 
   }
- 
+
+  
+
+
 }
