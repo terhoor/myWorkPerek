f94c83313f921549c9fdad28c519dbcdfe793696
terhoor <terhoor@yandex.ru>
2018-12-10 17:40:35 +0400

    add popup

---
+++ src/app/shared/components/popup/popup-info/popup-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-info',
+  templateUrl: './popup-info.component.html',
+  styleUrls: ['./popup-info.component.css']
+})
+export class PopupInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
