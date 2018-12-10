c0c293dcd13ca9bbe9bff0403e4e12e16eef3982
terhoor <terhoor@yandex.ru>
2018-12-10 17:19:22 +0400

    change reg access page

---
+++ src/app/registration-access-page/registration-access-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-access-page',
+  templateUrl: './registration-access-page.component.html',
+  styleUrls: ['./registration-access-page.component.css']
+})
+export class RegistrationAccessPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
